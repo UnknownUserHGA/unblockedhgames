2292ef0ee9cfe74bde2f2ff0551545526c8596d5
UnknownUserHGA <27talkingtonc@students.bcsc.k12.in.us>
2026-02-27 14:46:45 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
+        <Header title="You Just got hacked!" />
+        <p className="sorry you failed this test">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
